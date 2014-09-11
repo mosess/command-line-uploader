9429d24153b2ef4a0310c1cafae7a628c55cfe6f
Sean Katz <sean@for-each.com>
2014-09-11 17:37:13 +0300

    This fork is for a simpler command line uploader A. No signing process (we didn't need it so...) B. All the variables come for env variables so it can be used by Jenkins and alike C. Results are saved as variables for use by Jenkins

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 command-line-uploader
 =====================
+
+This fork is for a simpler command line uploader and yet ah
+A. No signing process (we didn't need it so...)
+B. All the variables come for env variables so it can be used by Jenkins and alike
+C. Results are saved as variables for use by Jenkins
--- testfairy-upload.sh
+++ testfairy-upload.sh
@@ -3,20 +3,20 @@
 UPLOADER_VERSION=1.08
 
 # Put your TestFairy API_KEY here. Find it in your TestFairy account settings.
-TESTFAIRY_API_KEY=
+TESTFAIRY_API_KEY=$TESTFAIRY_API_KEY
 
 # Tester Groups that will be notified when the app is ready. Setup groups in your TestFairy account testers page.
 # This parameter is optional, leave empty if not required
 TESTER_GROUPS=
 
 # Should email testers about neew version. Set to "off" to disable email notifications.
-NOTIFY="on"
+NOTIFY="off"
 
 # If AUTO_UPDATE is "on" all users will be prompt to update to this build next time they run the app
-AUTO_UPDATE="off"
+AUTO_UPDATE="$TESTFAIRY_AUTO_UPDATE"
 
 # The maximum recording duration for every test. 
-MAX_DURATION="10m"
+MAX_DURATION="30m"
 
 # Is video recording enabled for this build 
 VIDEO="on"
@@ -25,7 +25,7 @@ VIDEO="on"
 ICON_WATERMARK="on"
 
 # Comment text will be included in the email sent to testers
-COMMENT=""
+COMMENT="$TESTFAIRY_COMMENT"
 
 # Your Keystore, Storepass and Alias, the ones you use to sign your app.
 KEYSTORE=
@@ -42,8 +42,15 @@ JARSIGNER=jarsigner
 SERVER_ENDPOINT=http://app.testfairy.com
 
 usage() {
-	echo "Usage: testfairy-upload.sh APK_FILENAME"
-	echo
+    echo
+    echo "--------------------------------------------------------"
+	echo "Usage: testfairy-upload.sh and the rest in env variables:"
+    echo TESTFAIRY_API_KEY=$TESTFAIRY_API_KEY
+    echo TESTFAIRY_AUTO_UPDATE=$TESTFAIRY_AUTO_UPDATE
+    echo TESTFAIRY_COMMENT=$TESTFAIRY_COMMENT
+    echo TESTFAIRY_APK_FILENAME=$TESTFAIRY_APK_FILENAME
+    echo "--------------------------------------------------------"
+    echo
 }
 	
 verify_tools() {
@@ -63,25 +70,6 @@ verify_tools() {
 		echo "Could not run curl tool, please check settings"
 		exit 1
 	fi
-	
-	OUTPUT=$( ${JARSIGNER} -help 2>&1 | grep "verify" )
-	if [ $? -ne 0 ]; then
-		echo "Could not run jarsigner tool, please check settings"
-		exit 1
-	fi
-	
-	# Check 'zipalign' tool
-	OUTPUT=$( ${ZIPALIGN} 2>&1 | grep -i "Zip alignment" )
-	if [ $? -ne 0 ]; then
-		echo "Could not run zipalign tool, please check settings"
-		exit 1
-	fi
-
-	OUTPUT=$( ${KEYTOOL} -help 2>&1 | grep "keypasswd" )
-	if [ $? -ne 0 ]; then
-		echo "Could not run keytool tool, please check settings"
-		exit 1
-	fi
 }
 
 verify_settings() {
@@ -90,46 +78,30 @@ verify_settings() {
 		echo "Please update API_KEY with your private API key, as noted in the Settings page"
 		exit 1
 	fi
-
-	if [ -z "${KEYSTORE}" -o -z "${STOREPASS}" -o -z "{$ALIAS}" ]; then
-		usage
-		echo "Please update KEYSTORE, STOREPASS and ALIAS with your jar signing credentials"
-		exit 1
-	fi
-
-	# verify KEYSTORE, STOREPASS and ALIAS at once
-	OUTPUT=$( ${KEYTOOL} -list -keystore "${KEYSTORE}" -storepass "${STOREPASS}" -alias "${ALIAS}" 2>&1 )
-	if [ $? -ne 0 ]; then
-		usage
-		echo "Please check keystore credentials; keytool failed to verify storepass and alias"
-		exit 1
-	fi
 }
 
-if [ $# -ne 1 ]; then
-	usage
-	exit 1
-fi
+###############
+
+usage
 
 # before even going on, make sure all tools work
 verify_tools
 verify_settings
 
-APK_FILENAME=$1
-if [ ! -f "${APK_FILENAME}" ]; then
+# temporary file paths
+DATE=`date`
+TESTFAIRY_INSTRUMENTED_APK=.testfairy.upload.apk
+rm -f "${TESTFAIRY_INSTRUMENTED_APK}"
+
+if [ ! -f "${TESTFAIRY_APK_FILENAME}" ]; then
 	usage
-	echo "Can't find file: ${APK_FILENAME}"
+	echo "Can't find file: ${TESTFAIRY_APK_FILENAME}"
 	exit 2
 fi
 
-# temporary file paths
-DATE=`date`
-TMP_FILENAME=.testfairy.upload.apk
-ZIPALIGNED_FILENAME=.testfairy.zipalign.apk
-rm -f "${TMP_FILENAME}" "${ZIPALIGNED_FILENAME}"
-
-/bin/echo -n "Uploading ${APK_FILENAME} to TestFairy.. "
-JSON=$( ${CURL} -s ${SERVER_ENDPOINT}/api/upload -F api_key=${TESTFAIRY_API_KEY} -F apk_file=@${APK_FILENAME} -F icon-watermark="${ICON_WATERMARK}" -F video="${VIDEO}" -F max-duration="${MAX_DURATION}" -F comment="${COMMENT}" -A "TestFairy Command Line Uploader ${UPLOADER_VERSION}" )
+# Upload to testfairy
+/bin/echo -n "Uploading ${TESTFAIRY_APK_FILENAME} to TestFairy.. "
+JSON=$( ${CURL} -s ${SERVER_ENDPOINT}/api/upload -F api_key=${TESTFAIRY_API_KEY} -F apk_file=@${TESTFAIRY_APK_FILENAME} -F icon-watermark="${ICON_WATERMARK}" -F video="${VIDEO}" -F max-duration="${MAX_DURATION}" -F comment="${COMMENT}" -A "TestFairy Command Line Uploader ${UPLOADER_VERSION}" )
 
 URL=$( echo ${JSON} | sed 's/\\\//\//g' | sed -n 's/.*"instrumented_url"\s*:\s*"\([^"]*\)".*/\1/p' )
 if [ -z "${URL}" ]; then
@@ -139,49 +111,16 @@ if [ -z "${URL}" ]; then
 	exit 1
 fi
 
-URL="${URL}?api_key=${TESTFAIRY_API_KEY}"
-
-echo "OK!"
-/bin/echo -n "Downloading instrumented APK.. "
-${CURL} -L -o ${TMP_FILENAME} -s ${URL}
-
-if [ ! -f "${TMP_FILENAME}" ]; then
-	echo "FAILED!"
-	echo
-	echo "Could not download APK back from server, please contact support@testfairy.com"
-	exit 1
-fi
+echo
+echo "FYI: Result from server is:"
+echo
+echo ${JSON}
+echo
 
 echo "OK!"
 
-/bin/echo -n "Re-signing APK file.. "
-${ZIP} -qd ${TMP_FILENAME} 'META-INF/*'
-${JARSIGNER} -keystore "${KEYSTORE}" -storepass "${STOREPASS}" -digestalg SHA1 -sigalg MD5withRSA ${TMP_FILENAME} "${ALIAS}"
-${JARSIGNER} -verify ${TMP_FILENAME} >/dev/null
-if [ $? -ne 0 ]; then
-	echo "FAILED!"
-	echo
-	echo "Jarsigner failed to verify, please check parameters and try again"
-	exit 1
-fi
-
-${ZIPALIGN} -f 4 ${TMP_FILENAME} ${ZIPALIGNED_FILENAME}
-rm -f ${TMP_FILENAME}
-echo "OK!"
-
-/bin/echo -n "Uploading signed APK to TestFairy.. "
-JSON=$( ${CURL} -s ${SERVER_ENDPOINT}/api/upload-signed -F api_key=${TESTFAIRY_API_KEY} -F apk_file=@${ZIPALIGNED_FILENAME} -F testers-groups="${TESTER_GROUPS}" -F auto-update="${AUTO_UPDATE}" -F notify="${NOTIFY}")
-rm -f ${ZIPALIGNED_FILENAME}
-
-URL=$( echo ${JSON} | sed 's/\\\//\//g' | sed -n 's/.*"build_url"\s*:\s*"\([^"]*\)".*/\1/p' )
-if [ -z "$URL" ]; then
-	echo "FAILED!"
-	echo
-	echo "Build uploaded, but no reply from server. Please contact support@testfairy.com"
-	exit 1
-fi
-
-echo "OK!"
+export TESTFAIRY_INSTRUMENTED_APK_URL="${URL}?api_key=${TESTFAIRY_API_KEY}"
 echo
 echo "Build was successfully uploaded to TestFairy and is available at:"
-echo ${URL}
+echo ${TESTFAIRY_INSTRUMENTED_APK_URL}
+
